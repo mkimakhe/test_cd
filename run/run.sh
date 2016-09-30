ce312d184e99fd64a27055ffe52cda73e6c809ad
Mehdi Kimakhe <mehdi.kimakhe@first-utility.com>
2016-09-30 18:49:46 +0100

    comment all the code

--- run/run.sh
+++ run/run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-env_conf_file=$1
-source $env_conf_file
-export project_folder=$(dirname $(dirname $(readlink -f "$0")))
+# env_conf_file=$1
+# source $env_conf_file
+# export project_folder=$(dirname $(dirname $(readlink -f "$0")))
 
-echo $project_folder
-echo $staging_database
-echo $processing_database
-echo $datamart_database
+# echo $project_folder
+# echo $staging_database
+# echo $processing_database
+# echo $datamart_database
