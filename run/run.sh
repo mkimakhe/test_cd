62950e29560e2c7f638b01371abe8fa9ed984a31
Mehdi KIMAKHE <mehdi.kimakhe@gmail.com>
2016-09-30 18:59:44 +0100

    uncomment 3rd line

--- run/run.sh
+++ run/run.sh
@@ -2,7 +2,7 @@
 
 env_conf_file=$1
 source $env_conf_file
-# export project_folder=$(dirname $(dirname $(readlink -f "$0")))
+export project_folder=$(dirname $(dirname $(readlink -f "$0")))
 
 # echo $project_folder
 # echo $staging_database
