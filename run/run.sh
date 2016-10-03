6fad3cb21c1f5f2bc9a3d962c7735b23a89494be
Mehdi KIMAKHE <mehdi.kimakhe@gmail.com>
2016-10-03 10:05:44 +0100

    uncomment 5th line

--- run/run.sh
+++ run/run.sh
@@ -5,6 +5,6 @@ source $env_conf_file
 export project_folder=$(dirname $(dirname $(readlink -f "$0")))
 
 echo $project_folder
-# echo $staging_database
+echo $staging_database
 # echo $processing_database
 # echo $datamart_database
