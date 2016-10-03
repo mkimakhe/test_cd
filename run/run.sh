8f79f8a56a90f35fd150ba1403898bfd4dc0014d
Mehdi KIMAKHE <mehdi.kimakhe@gmail.com>
2016-10-03 13:19:11 +0100

    commit 15

--- run/run.sh
+++ run/run.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-
 env_conf_file=$1
-
 source $env_conf_file
 export project_folder=$(dirname $(dirname $(readlink -f "$0")))
-
 
 echo $project_folder
 echo $staging_database
